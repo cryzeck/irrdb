83344ffc2e2be380ed1505009e2bbc1be9f90dff
Jan Astrup <jesastrup@gmail.com>
2020-07-27 21:04:39 +0200

    Working version

--- irrdb.go
+++ irrdb.go
@@ -2,53 +2,76 @@ package irrdb
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"net"
 	"regexp"
+	"strings"
 )
 
-func doquery(connect, args string) {
+func doquery(connect, args string) ([]string, error) {
 	version := "go-irrdb 0.1"
 
 	CONNECT := connect
 	c, err := net.Dial("tcp", CONNECT)
+
 	if err != nil {
 		fmt.Println(err)
-		return
+		return nil, err
 	}
+
 	fmt.Fprintf(c, "!!\n")
 	fmt.Fprintf(c, fmt.Sprintf("!n %s\n", version))
-	message, _ := bufio.NewReader(c).ReadString('C')
-	if message == "C" {
-		fmt.Println("Got C from server.")
-	} else {
-		log.Fatal(fmt.Sprintf("Got: %s from server.", message))
+	r := bufio.NewReader(c)
+	message, _ := r.ReadString('\n')
+
+	if message != "C\n" {
+		return nil, errors.New(fmt.Sprintf("Got: %s from server.", message))
 	}
+
 	fmt.Fprintf(c, args+"\n")
-	req, _ := bufio.NewReader(c).ReadString('C')
-	fmt.Println(req)
-	return
+	for {
+		req, _ := r.ReadString('\n')
+		if req == "D\n" {
+			return nil, errors.New("No prefies received.")
+		}
+		req, _ = r.ReadString('\n')
+		return strings.Split(strings.TrimSuffix(req, "\n"), " "), nil
+		log.Fatal(req)
+	}
 }
 
-func getquery(res string) string {
-	var autnum = regexp.MustCompile(`^\d{1,10}$`)
-	var asset = regexp.MustCompile(`AS-[A-Za-z0-9]+`)
+func getquery(res, ver string) (string, error) {
+	var autnum = regexp.MustCompile(`^AS\d{1,10}$`)
+	var asset = regexp.MustCompile(`^AS-[A-Za-z0-9]+$`)
 
 	if autnum.MatchString(res) {
-		return fmt.Sprintf("!6as%s", res)
+		if ver == "6" {
+			return fmt.Sprintf("!6%s", strings.ToLower(res)), nil
+		}
+		return fmt.Sprintf("!g%s", strings.ToLower(res)), nil
 	}
-
 	if asset.MatchString(res) {
-		return fmt.Sprintf("!a6%s", res)
+		if ver == "6" {
+			return fmt.Sprintf("!a6%s", res), nil
+		}
+		return fmt.Sprintf("!a4%s", res), nil
 	}
-	return "0"
+	return "0", errors.New(fmt.Sprintf("%s: invalid aut-num or as-set.", res))
 }
 
-func Queryv6(s, res string) {
-	doquery(s, getquery(res))
-}
+func Query(s, res, ver string) ([]string, error) {
 
-func Queryv4(s, res string) {
-	doquery(s, "!a6"+res) //!a4+res - !gas+res
+	if !(ver == "4" || ver == "6") {
+		return nil, errors.New("IP version need to be 4 or 6")
+	}
+
+	query, err := getquery(res, ver)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return doquery(s, query)
 }
